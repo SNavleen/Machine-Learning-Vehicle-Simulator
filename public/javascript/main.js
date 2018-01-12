4a2dd1d66a8698b0121a1312f8baaafa94cc61a1
Navleen <navleen95@gmail.com>
2018-01-12 12:56:36 -0500

    updated the main.js file

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,6 @@
 var primaryCtx = document.getElementById("canvas").getContext("2d");
 var secondaryCanvas = document.createElement("canvas"),
-      secondaryCtx = secondaryCanvas.getContext("2d");
-
-(function drawFrame() {
-
-})();
+    secondaryCtx = secondaryCanvas.getContext("2d");
 
 
 //function for what happens when the page loads
@@ -18,14 +14,9 @@ window.onload = function() {
 
 //calls the movement of the objects function and redisplays the objects
 function refresh() {
-  display();
-}
-
-//how everything should be displayed
-function display() {
-  requestAnimationFrame(display);
-  primaryCtx.save(); //Freeze redraw
-  drawGrid();
-  drawCars();
-  primaryCtx.restore(); //And now do the redraw
+    requestAnimationFrame(display);
+    primaryCtx.save(); //Freeze redraw
+    drawGrid();
+    drawCars();
+    primaryCtx.restore(); //And now do the redraw
 }
