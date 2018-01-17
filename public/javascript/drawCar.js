b40638abc6361ca5350230d085b7c72eb0f0d46e
Hassaan Malik <shanu1994@hotmail.com>
2018-01-17 15:59:33 -0500

    added orientation

--- public/javascript/drawCar.js
+++ public/javascript/drawCar.js
@@ -3,8 +3,12 @@ function drawCars(){
     socket.on('DumbCarArray',function(data){
       console.log(data);
       for (var i = 0; i < data.length; i++) {
+      	primaryCtx.save(); 
         primaryCtx.fillStyle = data[i].carColour;
+        primaryCtx.rotate(orientation*Math.PI/180);
+        var orientation = 20; //orientation will be changed to the cars orientation that nav will send
         primaryCtx.fillRect(data[i]._xPos*100,data[i]._yPos*100,30,20);
+        primaryCtx.restore();
       }
     });
     socket.on('RunCar',function(data){});
