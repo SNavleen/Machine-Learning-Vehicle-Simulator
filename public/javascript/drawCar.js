d76a5eed2b2b6d498551f204f1aa06f284f5f713
Paul Warnick <warnicpj@mcmaster.ca>
2018-01-18 15:54:52 -0500

    Fixed cars to clear after movement change

--- public/javascript/drawCar.js
+++ public/javascript/drawCar.js
@@ -3,11 +3,13 @@ function drawCars(){
     socket.on('DumbCarArray',function(data){
     	console.log("Dumb Car Array Received");
         console.log(data);
+        primaryCtx.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid();
       for (var i = 0; i < data.length; i++) {
-        //primaryCtx.clearRect(0, 0, canvas.width, canvas.height);
         primaryCtx.fillStyle = data[i].carColour;
         primaryCtx.fillRect(data[i]._xPos*100,data[i]._yPos*100,30,20);
       }
+
     });
     socket.on('RunCar',function(data){
     	console.log("RunCar");
