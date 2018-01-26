de04c2495fb6d7173995934bb3c53352649bf802
Trevor Rae <raetd@mcmaster.ca>
2018-01-26 16:30:02 -0500

    rotate

--- views/dcMovement.js
+++ views/dcMovement.js
@@ -33,42 +33,40 @@ module.exports = function(io) {
 
         // Variables for tracking if all cars have reached destination
        // var movementFinished = false;
-
         // Loop for moving cars
-        var carsFinished = true;
+        var carFinished = true;
+        var speedflag = false;
+        var tempspeed = 0;
         var dcMovementLoop = setInterval(function() { // Temporarily using interval to display cars moving slowly
             // This loop checks each car in carArray and moves it closer towards its destination
             for (var i = 0; i < carArray.length; i++) {
-                 console.log("x",carArray[i]._xPos,"y",carArray[i]._yPos,"speed:",carArray[i]._speed);
-                 console.log("xdes",carArray[i].xDestination,"ydes",carArray[i].yDestination,"speed:",carArray[i]._speed);
+
+               //  console.log("x",carArray[i]._xPos,"y",carArray[i]._yPos,"speed:",carArray[i]._speed);
+                 //console.log("xdes",carArray[i].xDestination,"ydes",carArray[i].yDestination,"speed:",carArray[i]._speed);
 
                 var xpos = precisionRound(carArray[i]._xPos,3);
                 var ypos = precisionRound(carArray[i]._yPos,3);
                 var speed = precisionRound(carArray[i]._speed,3);
                 var xdes = precisionRound(carArray[i].xDestination,3);
                 var ydes = precisionRound(carArray[i].yDestination,3);
+                // console.log(xpos,ypos,carArray[i]._orientation,carArray[i]._speed);
                 //determines if a car has reached its destination or not
-                carsFinished = true;
+                carFinished = true;
                 //checks if car is approaching destination
                 var slow = false;
               //checks if the car needs to move along the xaxis
               if (difference(xpos,xdes)>0.0001){
-                //checks if the car is approaching the destination
-                //console.log("xpos", xpos);
-                //console.log("xdest", xdes);
-                //console.log("speed", speed);
 
-                // console.log("difference in x " ,difference(xpos,xdes));
-                // console.log("add fact speed" ,addfact(speed));
                 if(difference(xpos,xdes) <= addfact(speed)){
-                  //console.log("tresdfgsdfgsdfgdfsgsdfgsdfgdfsgsdfgdsfgsdft");
-                  //decrements speed
+
                   carArray[i]._speed = precisionRound(speed - 0.01,3);
                   slow=true;
                 }
 
                 if(xpos > xdes){
-                  carArray[i]._orientation = 0;
+                 // console.log("HEEERRREE");
+                  carArray[i]._orientation = 180;
+
                   carArray[i]._xPos = precisionRound(xpos - speed,3);
                 }
                 else{
@@ -80,15 +78,47 @@ module.exports = function(io) {
 
               else if(difference(ypos,ydes)>0.0001){
                 //checks if the car is approaching the destination
-                if(difference(ypos,ydes) <= addfact(speed)){
+                if((difference(ypos,ydes) <= addfact(speed)) && speedflag == false){
                   //decrements speed
                   carArray[i]._speed = precisionRound(speed - 0.01,3);
                   slow=true;
                 }
 
                 if(ypos > carArray[i].yDestination){
-                  carArray[i]._orientation = 90;
-                  carArray[i]._yPos = precisionRound(ypos - speed,3);
+                  if(carArray[i]._orientation != 90){
+                    console.log("!------ CASE 1");
+
+                    //if(speedflag==false){
+                      //tempspeed = precisionRound((carArray[i]._speed/9),9);
+                    //}
+                    speedflag = true;
+                    carArray[i]._speed = 0.001111111111;
+                    speed = 0.001111111111;
+                    if(carArray[i]._orientation > 90){
+                      carArray[i]._orientation = carArray[i]._orientation - 10;
+                    }
+                    else{
+                      carArray[i]._orientation = carArray[i]._orientation + 10;
+                    }
+
+                  }
+                  else if(speedflag == true){
+                    console.log("!------ CASE 2  ");
+                   // carArray[i]._speed = precisionRound((tempspeed*9,9));
+                    carArray[i]._speed = 0.01;
+                    speed = 0.01;
+                    carArray[i]._orientation = 90;
+                    speedflag = false;
+                    
+                  }
+                  else{
+                    console.log("!------ CASE 3");
+
+                    carArray[i]._orientation = 90;
+                  }
+                 console.log(xpos,ypos,carArray[i]._orientation,carArray[i]._speed,slow);
+
+                  carArray[i]._yPos = precisionRound(ypos - carArray[i]._speed,3);
                 }
                 else{
                   carArray[i]._orientation = 270;
@@ -97,23 +127,20 @@ module.exports = function(io) {
                 carFinished = false;
               }
 
-              if(carArray[i]._speed < 0.05 && slow == false){
+              if(carArray[i]._speed < 0.05 && slow == false && speedflag == false){
+                console.log("speed uo", speed);
                   carArray[i]._speed = precisionRound(speed + 0.01,3);
+                  console.log("current speed",carArray[i]._speed);
               }
             }
+
           if(carFinished == true){
            carArray.splice(i,1);
           }
 
-
-
-
-
-
-
-
             // Updates the carArray with new positions and sends data to client
             carCreation.setCarArr(carArray);
+
             dcSocket.emit('DumbCarArray', carCreation.getFrontendCarArr());
         }, 100); // How often the server updates the client
     });
